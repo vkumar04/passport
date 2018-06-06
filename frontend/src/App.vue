da4231e3e2a6b1b94fa58ac2ca89f5e3fd64322f
Vick Kumar <vick.kumar44@gmail.com>
2018-06-06 17:43:45 -0400

    using axios to get api data

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
   </div>
 </template>
 
@@ -24,8 +9,18 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      api_data: []
     }
+  },
+  methods: {
+    getData(){
+      axios.get('localhost:5000/api').then((res) => {
+        this.api_data = res.data
+      })
+    }
+  },
+  mounted() {
+    this.getData
   }
 }
 </script>
